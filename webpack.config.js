ddd6d0146cef653a3946d7928d88e888c1c25bb1
Simone Bembi <wufe92@gmail.com>
2016-12-07 02:26:54 +0100

    Added babel transpilation from es6 to es5

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'ts-loader'
+                loader: 'babel-loader!ts-loader'
             },
             {
                 test: /\.json$/,
