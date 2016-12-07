d18b3215fd859fed0a6e3e2f5b9b39f5105c85b6
Simone Bembi <wufe92@gmail.com>
2016-12-07 03:03:50 +0100

    Added deepExtend function

--- src/main/Utils.ts
+++ src/main/Utils.ts
@@ -2,8 +2,30 @@ export default class Utils{
 
 	constructor(){}
 
-	static test(): string{
-		return "lol";
+	static deepExtend( ...sources: any[] ): any{
+		if( sources.length == 1 )
+			return sources[ 0 ];
+		if( sources.length == 0 )
+			return;
+		let source: any = Object.assign({}, sources[0]);
+		for( let i = 1; i < sources.length; i++ ){
+			let add: any = sources[i];
+			if( !( typeof add == 'object' ) ){
+				source = add;
+			}else if( typeof add == 'object' ){
+				add = Object.assign({}, add);
+				if( !( add instanceof Array ) ){
+					for( let x in add ){
+						source[x] = Utils.deepExtend(source[x], add[x]);
+					}
+				}else{
+					for( let a = 0; a < add.length; a++ ){
+						source[a] = Utils.deepExtend(source[a], add[a]);
+					}
+				}
+			}
+		}
+		return source;
 	}
 
 }
--- test/main/Utils.ts
+++ test/main/Utils.ts
@@ -11,8 +11,53 @@ describe( 'Utils', () => {
 	it( 'should be a class', () => {
 		Should( typeof Utils ).be.exactly( "function" );
 	});
-	describe( 'test', () => {
-		let test: string = Utils.test();
-		test.should.be.exactly( "lol" );
-	})
+	describe( 'deepExtend', () => {
+		it( 'should extend an object given 6 sources', () => {
+			let a: any = {
+				a: 'a',
+				b: {
+					c: 'c',
+					d: 'd'
+				}
+			};
+
+			let b: any = {
+				b: {
+					d: 'asd'
+				}
+			};
+
+			let c: any = {
+				b: {
+					d: 'lol',
+					e: 'asd'
+				}
+			};
+
+			let d: any = {
+				b: false
+			};
+
+			let e: any = {
+				b: {
+					d: 't1',
+					e: 't2'
+				}
+			};
+
+			let f: any = {
+				b: {
+					c: {
+						z: 'end',
+						u: false
+					}
+				}
+			};
+			let ext: any = Utils.deepExtend( a, b, c, d, e, f );
+			Should( ext ).have.propertyByPath( 'b', 'c', 'z' ).which.is.equal( 'end' );
+			Should( ext ).have.propertyByPath( 'b', 'd' ).which.is.equal( 't1' );
+			Should( ext ).have.propertyByPath( 'b', 'e' ).which.is.equal( 't2' );
+			Should( ext ).have.propertyByPath( 'b', 'c', 'u' ).which.is.equal( false );
+		});
+	});
 });
